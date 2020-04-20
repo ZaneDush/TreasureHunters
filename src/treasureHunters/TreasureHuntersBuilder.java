6f6d81f95828f651a8f88c1514ed74c66099429a
Zane Dush <z7112432@yahoo.com>
2020-04-20 16:38:45 -0500

    Added parameters to display so they can be easily manipulated

--- src/treasureHunters/TreasureHuntersBuilder.java
+++ src/treasureHunters/TreasureHuntersBuilder.java
@@ -31,26 +31,26 @@ Object> {
 		NetworkBuilder<Object> netBuilder = new NetworkBuilder<Object>(
 				"team network", context, true);
 		netBuilder.buildNetwork();
-		//		Parameters params = RunEnvironment.getInstance().getParameters();
-		//		int areaDimensions = (Integer) params.getValue("area_dimensions");
-		int areaDimensions = 100;
+		Parameters params = RunEnvironment.getInstance().getParameters();
+		int areaDimensions = (Integer) params.getValue("area_dimensions");
+		//		int areaDimensions = 100;
 		GridFactory gridFactory = GridFactoryFinder.createGridFactory(null);
 		Grid<Object> grid = gridFactory.createGrid("grid", context,
 				new GridBuilderParameters<Object>(new WrapAroundBorders(),
 						new SimpleGridAdder<Object>(), true, areaDimensions, areaDimensions)); // true so that many agents may occupy the same location at a time
-		//		int explorerCount = (Integer) params.getValue("explorer_count");
-		//		double navigationMemory = (Double) params.getValue("navigation_memory");
-		//		int perceptionRadius = (Integer) params.getValue("perception_radius");
-		//		int treasureCount = (Integer) params.getValue("treasure_count");
-		//		double treasureValue = (Double) params.getValue("treasure_value");
-		//		double treasureDecayRate = (Double) params.getValue("treasure_decay_rate");
-		//		int minDistance = (Integer) params.getValue("minimum_distance_from_treasure");
-		int explorerCount = 100;
-		double navigationMemory = .80;
-		int perceptionRadius = 3;
-		int treasureCount = 10;
-		double treasureValue = 1000;
-		double treasureDecayRate = .10;
+		int explorerCount = (Integer) params.getValue("explorer_count");
+		double navigationMemory = (Double) params.getValue("navigation_memory");
+		int perceptionRadius = (Integer) params.getValue("perception_radius");
+		int treasureCount = (Integer) params.getValue("treasure_count");
+		double treasureValue = (Double) params.getValue("treasure_value");
+		double treasureDecayRate = (Double) params.getValue("treasure_decay_rate");
+		//int minDistance = (Integer) params.getValue("minimum_distance_from_treasure");
+		//		int explorerCount = 100;
+		//		double navigationMemory = .80;
+		//		int perceptionRadius = 3;
+		//		int treasureCount = 10;
+		//		double treasureValue = 1000;
+		//		double treasureDecayRate = .10;
 		// Create Explorers
 		for (int i = 0; i < explorerCount; i++) {
 			context.add(new Explorer(grid, navigationMemory, perceptionRadius, treasureCount, treasureValue, treasureDecayRate));
@@ -66,35 +66,35 @@ Object> {
 			int y = RandomHelper.nextIntFromTo(0, areaDimensions - 1);
 			grid.moveTo(obj, x, y);
 		}
-//		List<List<Integer>> treasureLocations = new ArrayList<List<Integer>>();
-//		for (Object obj : context) {
-//			//NdPoint pt = space.getLocation(obj);
-//			if (obj instanceof Treasure) {
-//				List<Integer> treasurePoint = new ArrayList<Integer>();
-//				int x = RandomHelper.nextIntFromTo(0, areaDimensions - 1);
-//				int y = RandomHelper.nextIntFromTo(0, areaDimensions - 1);
-//				treasurePoint.add(x);
-//				treasurePoint.add(y);
-//				grid.moveTo(obj, x, y);
-//			}
-//		}
-//		for (Object obj : context) {
-//			//NdPoint pt = space.getLocation(obj);
-//			if (obj instanceof Explorer) {
-//				boolean inLoop = true;
-//				while (inLoop) {
-//					int x = RandomHelper.nextIntFromTo(0, areaDimensions - 1);
-//					int y = RandomHelper.nextIntFromTo(0, areaDimensions - 1);
-//					for (List<Integer> treasureLocation : treasureLocations) {
-//						if ((x - treasureLocation.get(0)) >= minDistance && (y - treasureLocation.get(1)) >= minDistance) {
-//							grid.moveTo(obj, x, y);
-//							inLoop = false;
-//						}
-//					}
-//				}
-//				
-//			}
-//		}
+		//		List<List<Integer>> treasureLocations = new ArrayList<List<Integer>>();
+		//		for (Object obj : context) {
+		//			//NdPoint pt = space.getLocation(obj);
+		//			if (obj instanceof Treasure) {
+		//				List<Integer> treasurePoint = new ArrayList<Integer>();
+		//				int x = RandomHelper.nextIntFromTo(0, areaDimensions - 1);
+		//				int y = RandomHelper.nextIntFromTo(0, areaDimensions - 1);
+		//				treasurePoint.add(x);
+		//				treasurePoint.add(y);
+		//				grid.moveTo(obj, x, y);
+		//			}
+		//		}
+		//		for (Object obj : context) {
+		//			//NdPoint pt = space.getLocation(obj);
+		//			if (obj instanceof Explorer) {
+		//				boolean inLoop = true;
+		//				while (inLoop) {
+		//					int x = RandomHelper.nextIntFromTo(0, areaDimensions - 1);
+		//					int y = RandomHelper.nextIntFromTo(0, areaDimensions - 1);
+		//					for (List<Integer> treasureLocation : treasureLocations) {
+		//						if ((x - treasureLocation.get(0)) >= minDistance && (y - treasureLocation.get(1)) >= minDistance) {
+		//							grid.moveTo(obj, x, y);
+		//							inLoop = false;
+		//						}
+		//					}
+		//				}
+		//				
+		//			}
+		//		}
 		//		
 		//		if (RunEnvironment.getInstance().isBatch()) {
 		//			RunEnvironment.getInstance().endAt(20);
